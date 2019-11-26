34f9f15c742921b80a99ec3dd39188ec253f5ee9
Colm Tang <colm.tang@ucdconnect.ie>
2019-11-26 11:14:45 +0000

    halfway through Q4

--- guess_it.rb
+++ guess_it.rb
@@ -1,26 +1,30 @@
 require_relative 'oracle.rb'
 require_relative 'random_player.rb'
 require_relative 'smart_player.rb'
+require_relative 'sequential_player.rb'
 
 class Guess_It
   def initialize
     @oracle = Oracle.new(100)
     @player_r = Random_Player.new("Ako",@oracle,100)
     @player_s = Smart_Player.new("Jane",@oracle,100)
+    @player_seq = Sequential_Player.new("Thien",@oracle,100)
+    @players = [@player_r,@player_s,@player_seq]
   end
 
   def play_game
-    finished = false
+    finished = [false,false,false]
+    indicator = 0
     puts "secret number is :#{@oracle.secret}"
     until finished
-      if @player_r.guess
-        finished = true
-      end
-    end
-    finished = false
-    until finished
-      if @player_s.guess
-        finished = true
+      indicator = indicator % 3
+      @players.each do |player|
+        unless finished[indicator]
+          if player.guess
+            finished[indicator] = true
+          end
+        end
+        indicator += 1
       end
     end
     puts "#{@player_r.name} made #{@player_r.no_of_guesses}"
--- sequential_player.rb
+++ sequential_player.rb
@@ -1,5 +1,15 @@
-
+require_relative 'player.rb'
 
 class Sequential_Player < Player
-  
+  def initialize(name,oracle,range)
+    super(name,oracle,range)
+  end
+
+  def guess
+    player_guess = 1
+    @no_of_guesses += 1
+    if @oracle.is_it?(player_guess) == true
+      true
+    end
+  end
 end
